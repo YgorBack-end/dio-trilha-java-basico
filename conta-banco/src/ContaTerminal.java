d4e94665b56486794fe2e15f7e95eed0259336d6
ygor silva <ygorsilvadev@gmail.com>
2025-07-06 18:19:12 -0300

    primeiro commit do projeto conta-banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta: "); 
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o numero da agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo é de R$" + saldo + " já está disponível para saque.";
+
+        System.out.println(mensagem);
+
+        scanner.close();
+
+
+
     }
 }
